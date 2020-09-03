bc4ddc3977b9168bb6b00cd5ca18c226fb59e64e
aki <aki@chainlogis.co.kr>
2020-09-03 23:58:47 +0900

    폰트 적용

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,9 +4,30 @@ import { createGlobalStyle } from "styled-components";
 
 import Main from "./pages/Main";
 
+import MonumentGroteskRegularWoff from "./assets/fonts/MonumentGrotesk-Regular.woff";
+import MonumentGroteskRegularTtf from "./assets/fonts/MonumentGrotesk-Regular.ttf";
+import MonumentGroteskRegularOtf from "./assets/fonts/MonumentGrotesk-Regular.otf";
+
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'MonumentGrotesk Regular';
+    src: local('MonumentGrotesk Regular'),
+        url(${MonumentGroteskRegularWoff}) format('woff'),
+        url(${MonumentGroteskRegularTtf}) format('ttf'),
+        url(${MonumentGroteskRegularOtf}) format('eot');
+  }
+
+  body {
+    background-color: #000;
+    color: #fff;
+    padding: 0;
+    margin: 0;
+  }
+
   *, :after, :before {
     box-sizing: border-box;
+    font-family: 'MonumentGrotesk Regular', AppleSDGothicNeo, sans-serif;
+    
   }
 `;
 
