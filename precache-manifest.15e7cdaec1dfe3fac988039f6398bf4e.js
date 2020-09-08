7fb85b5b8de131edfd644b5a233455ffb2a533ef
aki <aki@chainlogis.co.kr>
2020-09-09 02:50:28 +0900

    Updates

---
+++ precache-manifest.15e7cdaec1dfe3fac988039f6398bf4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1549dded9fe41b86b95972a482d03ea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fdbaf43e3ec79125128",
+    "url": "/static/js/2.fcb57fe5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fcb57fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d924bbf395b3c82254a1",
+    "url": "/static/js/main.3ff3a67a.chunk.js"
+  },
+  {
+    "revision": "6406fa4029f2d80a4431",
+    "url": "/static/js/runtime-main.f76cf2e0.js"
+  },
+  {
+    "revision": "07a05a3ea4f90448523aeb8043f8d513",
+    "url": "/static/media/MonumentGrotesk-Regular.07a05a3e.woff"
+  },
+  {
+    "revision": "46ae4c717b0058f2a78a78575bff1a2a",
+    "url": "/static/media/MonumentGrotesk-Regular.46ae4c71.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81a769cf453680870cf41cd6189def05.js"
+  "/precache-manifest.15e7cdaec1dfe3fac988039f6398bf4e.js"
 );
 
 self.addEventListener('message', (event) => {
